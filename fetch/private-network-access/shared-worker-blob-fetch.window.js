6e3ae3d72631526319169e469377f23274069bc0
Jonathan Hao <phao@chromium.org>
2023-02-21 09:52:52 -0800

    [Private Network Access] Add shared worker blob fetch tests
    Similar to the worker blob fetch tests. However, PNA for shared workers
    is warning only, so tests that expect requests to be blocked will fail
    except in virtual/pna-workers-enabled.
    
    Bug: 1371454, 1382068
    Change-Id: I7a3dbebf8f33caaa1120e50d7876d8c16660c8e4

--- fetch/private-network-access/shared-worker-blob-fetch.window.js
+++ fetch/private-network-access/shared-worker-blob-fetch.window.js
@@ -3,20 +3,20 @@
 //
 // Spec: https://wicg.github.io/private-network-access/#integration-fetch
 //
-// These tests check that fetches from within `Worker` scripts loaded from blob
-// URLs are subject to Private Network Access checks, just like fetches from
-// within documents.
+// These tests check that fetches from within `SharedWorker` scripts that are
+// loaded from blob URLs are subject to Private Network Access checks, just like
+// fetches from within documents.
 //
 // This file covers only those tests that must execute in a non-secure context.
-// Other tests are defined in: worker-blob-fetch.https.window.js
+// Other tests are defined in: shared-worker-blob-fetch.https.window.js
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_LOCAL },
   target: { server: Server.HTTP_LOCAL },
   expected: WorkerFetchTestResult.SUCCESS,
 }), "local to local: success.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_PRIVATE },
   target: {
     server: Server.HTTP_LOCAL,
@@ -28,13 +28,13 @@ promise_test(t => workerBlobFetchTest(t, {
   expected: WorkerFetchTestResult.FAILURE,
 }), "private to local: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_PRIVATE },
   target: { server: Server.HTTP_PRIVATE },
   expected: WorkerFetchTestResult.SUCCESS,
 }), "private to private: success.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_PUBLIC },
   target: {
     server: Server.HTTP_LOCAL,
@@ -46,7 +46,7 @@ promise_test(t => workerBlobFetchTest(t, {
   expected: WorkerFetchTestResult.FAILURE,
 }), "public to local: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_PUBLIC },
   target: {
     server: Server.HTTP_PRIVATE,
@@ -58,13 +58,13 @@ promise_test(t => workerBlobFetchTest(t, {
   expected: WorkerFetchTestResult.FAILURE,
 }), "public to private: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTP_PUBLIC },
   target: { server: Server.HTTP_PUBLIC },
   expected: WorkerFetchTestResult.SUCCESS,
 }), "public to public: success.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: {
     server: Server.HTTP_LOCAL,
     treatAsPublic: true,
@@ -76,7 +76,7 @@ promise_test(t => workerBlobFetchTest(t, {
   expected: WorkerFetchTestResult.FAILURE,
 }), "treat-as-public to local: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: {
     server: Server.HTTP_LOCAL,
     treatAsPublic: true,
@@ -91,7 +91,7 @@ promise_test(t => workerBlobFetchTest(t, {
   expected: WorkerFetchTestResult.FAILURE,
 }), "treat-as-public to private: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: {
     server: Server.HTTP_LOCAL,
     treatAsPublic: true,
@@ -106,36 +106,36 @@ promise_test(t => workerBlobFetchTest(t, {
 // The following tests verify that workers served over HTTPS are not allowed to
 // make private network requests because they are not secure contexts.
 
-promise_test(t => workerBlobFetchTest(t, {
-  source: { server: Server.HTTPS_PRIVATE },
+promise_test(t => sharedWorkerBlobFetchTest(t, {
+  source: { server: Server.HTTPS_LOCAL },
   target: {
     server: Server.HTTP_LOCAL,
     behavior: {
-      preflight: PreflightBehavior.success(token()),
+      preflight: PreflightBehavior.optionalSuccess(token()),
+      response: ResponseBehavior.allowCrossOrigin(),
+    },
+  },
+  expected: WorkerFetchTestResult.FAILURE,
+}), "local https to local: failure.");
+
+promise_test(t => sharedWorkerBlobFetchTest(t, {
+  source: { server: Server.HTTPS_PUBLIC },
+  target: {
+    server: Server.HTTP_LOCAL,
+    behavior: {
+      preflight: PreflightBehavior.optionalSuccess(token()),
       response: ResponseBehavior.allowCrossOrigin(),
     },
   },
   expected: WorkerFetchTestResult.FAILURE,
 }), "private https to local: failure.");
 
-promise_test(t => workerBlobFetchTest(t, {
-  source: { server: Server.HTTPS_PUBLIC },
-  target: {
-    server: Server.HTTP_PRIVATE,
-    behavior: {
-      preflight: PreflightBehavior.success(token()),
-      response: ResponseBehavior.allowCrossOrigin(),
-    },
-  },
-  expected: WorkerFetchTestResult.FAILURE,
-}), "public https to private: failure.");
-
-promise_test(t => workerBlobFetchTest(t, {
+promise_test(t => sharedWorkerBlobFetchTest(t, {
   source: { server: Server.HTTPS_PUBLIC },
   target: {
     server: Server.HTTP_LOCAL,
     behavior: {
-      preflight: PreflightBehavior.success(token()),
+      preflight: PreflightBehavior.optionalSuccess(token()),
       response: ResponseBehavior.allowCrossOrigin(),
     },
   },
